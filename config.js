b2120d5297aa630ae4afe98025ffcaaa0d54bda5
hugn99it <89432485+hugn99it@users.noreply.github.com>
2021-09-07 14:37:21 +0700

    new

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Công chúa ơi!',
+    introDesc: 'Hôm nay là tròn 1 tháng chúng ta quen nhao rùi đóa. Sau bao nhiêu dui buồn chúng ta vẫn còn bên nhao, a có cái này muốn hỏi em nè ',
+    btnIntro: 'Dạaaa',
+    title: 'Em có yêu anh không? có muốn về nhà với anh không? ',
+    desc: 'Tình yêu anh xây đủ lớn để dù rời xa ta vẫn có nhau. ',
+    btnYes: 'Dạ, em yêu anhh <3',
+    btnNo: 'Hok thèm, xía',
+    question:'Hehe Tại sao nè',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Chỉ cần là anh thì không cóa lý doo <33333333',
+    mess: 'Anh biết mà 🥰.Anh cũng yêu emm công chúa ơi 😘😘',
+    messDesc: 'Hehe vậy từ giờ phải gọi anh là anh iu nhá.',
+    btnAccept: 'Dạ anh iuuu <3',
+    messLink: 'https://www.messenger.com/t/100009108473244' 
+}
